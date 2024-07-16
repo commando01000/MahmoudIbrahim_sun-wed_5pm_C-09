bd38dc9ff5f0521f7adb8071e6fea251d01c91db
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-17 01:04:08 +0300

    assignment

--- MahmoudIbrahim_sun&wed_5pm_C#09/Program.cs
+++ MahmoudIbrahim_sun&wed_5pm_C#09/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-
+            Maths operation = new Maths();
+            //operation.Add(5, 5);
+            //operation.Subtract(5, 5);
+            //operation.Multiply(5, 5);
+            //operation.Divide(5, 5);
+            Maths.Add(5, 2);
+            Maths.Subtract(1, 3);
+            Maths.Multiply(4, 2);
+            Maths.Divide(5, 5);
         }
     }
 }
